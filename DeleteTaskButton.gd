30d33b7004257eaa760b7d553911d2637a0e7854
Robert <robbas@gmail.com>
2023-01-07 12:43:15 +0100

    Fixed issue when deleting task
    Fixed the issue where deleting a task destroyed the relation between task button -> task.

--- DeleteTaskButton.gd
+++ DeleteTaskButton.gd
@@ -14,24 +14,31 @@ func _process(delta):
 	pass
 
 func _show_delete_dialog():
-	print("delete pressed")
 	var delete_dialog : ConfirmationDialog = ConfirmationDialog.new()
 	delete_dialog.title = "Delete task"
 	delete_dialog.dialog_text = "Are you sure you want to delete the task " + str(task.name) + "? This can't be undone."
 	add_child(delete_dialog)
 	delete_dialog.show()
 	
-	delete_dialog.get_ok_button().pressed.connect(_delete_task)
+	delete_dialog.get_ok_button().pressed.connect(_delete)
 	delete_dialog.get_cancel_button().pressed.connect(_delete_task_cancelled)
 	
-func _delete_task():
-	print("DELETE TASK")
+func _delete():
 	_delete_related_button(task)
+	_delete_task()
+
+func _delete_task():
+	var tasks = task_manager.get_tasks()
+	tasks.erase(task)
 	task.queue_free()
 
 func _delete_related_button(task):
 	var buttons = task_manager.get_task_buttons()
-	buttons[buttons.find(task)].queue_free() 
+	print(buttons)
+	for button in buttons:
+		if task.name == str(button.name).replace("_button", ""):
+			buttons.erase(button)
+			button.queue_free()
 
 func _delete_task_cancelled():
 	print("DELETE TASK CANCELLED")
